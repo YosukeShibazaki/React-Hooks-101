f9c628187b149ea1300b4f7f2242614db3af8bd0
YosukeShibazaki <tebasaki4839@gmail.com>
2020-07-10 23:28:49 +0900

    3-7複数の状態を管理しよう

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
 import React,{useState} from 'react';
 //import './App.css';
 
-const App = () => {
-  const [count,setCount] = useState(0); // stateの設定
-  const increment = () => setCount(count + 1); //　関数incrementを定義。countの中身をsetCountで更新。
-  const decrement = () => setCount(count - 1);
+const App = (props) => { // 仮にinitialStatesが外部から与えられるものならば、引数にpropsを宣言
+  const [name,setName] = useState(props.name);
+  const [price,setPrice] = useState(props.price);
 
-  const increment2 = () => setCount( counter => counter + 1);
-  const decrement2 = () => setCount( counter => counter - 1);
-
-  // 演習
-  const reset = () => setCount(0); // カウンターを初期化
-  const double = () => setCount(count * 2); // カウントを2倍
-  const therdSelect = () => setCount( counter => {
-    return counter % 3 === 0? counter / 3 : counter; // 三項演算子を使うと短く書ける。ifでも書けるが長くなる。
-  });
+  // リセットボタンの処理
+  // 実行時に外部のpropsの値をセットする
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  }
 
   return (
     <> {/* React.Fragmentの略 */}
-      <div>count:{count}</div>
-      <button onClick={() => increment()}>+</button>
-      <button onClick={() => decrement()}>-</button>
-
-      <div>
-        <button onClick={() => increment2()}>+</button>
-        <button onClick={() => decrement2()}>-</button>
-      </div>
-
-      <div>
-        <button onClick={() => reset()}>Reset</button>
-      </div>
-
-      <div>
-        <button onClick={() => double()}>Double</button>
-      </div>
-
-      <div>
-        <button onClick={() => therdSelect()}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>現在の{name}は{price}円です。</p>
+      <button onClick={()=>{setPrice(price + 1)}}>+</button>
+      <button onClick={()=>{setPrice(price - 1)}}>-</button>
+      <button onClick={()=>{reset()}}>Reset</button>
+      <input value={name}　onChange={(e)=>{setName(e.target.value)}} />
     </>
   );
+}
+
+// 外部からのpropsを初期値として定義
+App.defaultProps={
+  name:'サンプル',
+  price:1000
 }
 
 export default App;
