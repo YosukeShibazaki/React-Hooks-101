b2f160ec8e530db579c1e1c689be656f819daea4
YosukeShibazaki <tebasaki4839@gmail.com>
2020-07-11 12:07:11 +0900

    3-8object-and-useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React,{useState} from 'react';
 //import './App.css';
 
-const App = (props) => { // 仮にinitialStatesが外部から与えられるものならば、引数にpropsを宣言
-  const [name,setName] = useState(props.name);
-  const [price,setPrice] = useState(props.price);
-
-  // リセットボタンの処理
-  // 実行時に外部のpropsの値をセットする
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  }
+const App = (props) => { // initialStatesが外部から与えられるものならば、引数にpropsを宣言
+  const [state,setState] = useState(props); // 値ではなくオブジェクトを引数として入れることもできる
+   // オブジェクト丸ごと指定することで下記2行は必要なくなる
+  //const [name,setName] = useState(props.name);
+  //const [price,setPrice] = useState(props.price);
+  const {name,price} = state; // stateオブジェクトの中身を変数nameとpriceにそれぞれ代入
 
   return (
     <> {/* React.Fragmentの略 */}
       <p>現在の{name}は{price}円です。</p>
-      <button onClick={()=>{setPrice(price + 1)}}>+</button>
-      <button onClick={()=>{setPrice(price - 1)}}>-</button>
-      <button onClick={()=>{reset()}}>Reset</button>
-      <input value={name}　onChange={(e)=>{setName(e.target.value)}} />
+      <button onClick={()=>{setState({...state,price:price + 1})}}>+</button>
+      <button onClick={()=>{setState({...state,price:price - 1})}}>-</button>
+      <button onClick={()=> setState(props)}>Reset</button>
+      <input value={name}　onChange={(e)=>{setState({...state,name:e.target.value})}} />
+      {/* ...stateを展開。展開したオブジェクトの中のどのキーを書き換えるかを指定する */}
     </>
   );
 }
