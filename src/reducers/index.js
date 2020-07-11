520e2536e92812df06dcf5cf1f6d81e87e78a303
YosukeShibazaki <tebasaki4839@gmail.com>
2020-07-11 22:48:34 +0900

    3-12create-reducer-for-events

---
+++ src/reducers/index.js
@@ -0,0 +1,20 @@
+const events = ( state = [] , action ) => { // state = 【】とすると、stateの値を初期化することができる
+  switch (action.type) {
+    case 'CREATE_EVENT':
+      const event = { title:action.title , body:action.body };
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, {id, ...event}]; // id:idを省略するとidのみで書ける
+      break;
+    case 'DELETE_EVENT':
+      return state;
+      break;
+    case 'DELETE_ALL_EVENTS':
+      return [];
+      break;
+    default:
+      return state;
+  }
+}
+
+export default events;
