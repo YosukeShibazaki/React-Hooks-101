381c2f6588661de7983abc8496b932a83a19324b
YosukeShibazaki <tebasaki4839@gmail.com>
2020-07-27 23:52:21 +0900

    4-28delete-all-operationLogs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -50,6 +50,18 @@ const EventForm = () => {
     }
   }
 
+  // 全ての操作ログを削除する処理
+  const deleteAllOperationLogs = e => {
+    e.preventDefault();
+    const result = window.confirm('全ての操作ログを本当に削除しても良いですか？');
+
+    if (result){
+      dispatch({
+        type:DELETE_ALL_OPERATION_LOGS
+      })
+    }
+  }
+
   // イベント作成ボタンの活性・非活性を切り替えるための定数。真偽値を代入。
   const unCreatable = body === '' || title === '';
 
@@ -69,6 +81,7 @@ const EventForm = () => {
 
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全ての操作ログを削除する</button>
       </form>
     </>
   )
