edc898a495d29478ec6c701aab2baaab6e165413
YosukeShibazaki <tebasaki4839@gmail.com>
2020-08-01 20:57:08 +0900

    補足のメモ書きを追加しました

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,8 @@ const events = ( state = [] , action ) => { // state = 【】とすると、stat
     case 'CREATE_EVENT':
       const event = { title:action.title , body:action.body };
       const length = state.length;
-      const id = length === 0 ? 1 : state[length - 1].id + 1;
-      return [...state, {id, ...event}]; // id:idを省略するとidのみで書ける
+      const id = length === 0 ? 1 : state[length - 1].id + 1; // 渡ってきたstateの要素が0ならidに1を、そうじゃない場合は最後のstateの配列の最後の要素のid + 1の値を代入する
+      return [...state, {id, ...event}]; // id:idを省略するとidのみで書ける。定義した配列をオブジェクトとして展開。
     case 'DELETE_EVENT':
       return state;
     case 'DELETE_ALL_EVENTS':
