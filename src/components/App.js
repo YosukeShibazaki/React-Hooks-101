e0060b8ead25975d1a0392c25cd47197e0895f8e
YosukeShibazaki <tebasaki4839@gmail.com>
2020-08-01 14:01:34 +0900

    4-32save-state-with-localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React,{useReducer} from 'react';
+import React,{ useReducer, useEffect } from 'react';
 import reducer from '../reducers'; // jsファイルまで指定しなくてもimportができる
 //import './App.css';
 import EventForm from './EventForm';
@@ -7,12 +7,22 @@ import AppContext from '../contexts/AppContext';
 // createContextのProviderでretrun内をラッピングする。valueを指定する。(Provider側のコーディング)
 import OperationLogs from './OperationLogs';
 
+const APP_KEY = 'appWithRedux';
+
 const App = () => {
-  const initialState = {
+  const appState = localStorage.getItem(APP_KEY);
+  const initialState = appState ? JSON.parse(appState) : {
     events:[],
     operationLogs:[]
   }
-  const [state,dispatch] = useReducer(reducer,initialState); // reducerの定義する。dispatchにはactionのtypeを渡
+
+  const [state,dispatch] = useReducer(reducer,initialState); // reducerの定義をする。dispatchにはactionのtypeを渡す。
+
+  useEffect(() => {
+    // localStorage.setItem(第一引数,第二引数)とすることで、第一引数をキーにして第二引数をローカルストレージに保存できる。
+    // 詳細はMDNを参照
+    localStorage.setItem(APP_KEY,JSON.stringify(state))
+  },[state])
 
   return (
     <AppContext.Provider value={{ state,dispatch }}>
